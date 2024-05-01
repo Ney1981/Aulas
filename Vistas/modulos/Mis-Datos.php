a89dce40fefeae5ddb20c98fe14b284e2101fa87
Ney1981 <joanmartwhit500@gmail.com>
2024-04-30 19:29:46 -0500

    Maquetacio

--- Vistas/modulos/Mis-Datos.php
+++ Vistas/modulos/Mis-Datos.php
@@ -13,21 +13,22 @@
             <div class="box-body">
 
             <div style="background-color: #154360; padding: 20px;">
-            <div class="" style="background-color: #85C1E9; padding: 50px; border-radius: 10px;">
+                <div class="" style="background-color: #85C1E9; padding: 50px; border-radius: 10px;">
 
-                <form method="post" enctype="multipart/form-data">
+                    <form method="post" enctype="multipart/form-data">
 
-                    <div class="row">
+                        <div class="row">
 
-                        <?php
+                            <?php
 
-                        $perfil = new UsuariosC();
-                        $perfil -> VerPerfilC();
+                            $perfil = new UsuariosC();
+                            $perfil -> VerPerfilC();
 
-                        ?>
+                            ?>
 
-                    </div>
+                        </div>
 
+                </div>
             </div>
 
         <div class="box-footer">
@@ -44,7 +45,7 @@
             </form>
 
             </div>
-            </div>
+            
 
             </div>
 
